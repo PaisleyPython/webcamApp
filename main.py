5c3fb8e3d14aeb420de1994ac2fa0ff54f99edc0
PaisleyPython <148840962+PaisleyPython@users.noreply.github.com>
2023-12-06 11:45:38 +0000

    change file directory and start boiler plate code

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-# main
-print("Hello")
+# Boiler plate code (the code that we will always need when starting writing with a package type)
+
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+
+Builder.load_file('frontend.kv')
+
+
+class RootWidget(ScreenManager):
+    pass
+
+
+class MainApp(App):
+
+    def build(self):
+        return RootWidget()
+
+
+MainApp().run()
+
+# Boiler plate code end:-
+# ==================================================================================================
