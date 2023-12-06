bb17aba4b4904de1f1d8bbf497203986644a9505
PaisleyPython <148840962+PaisleyPython@users.noreply.github.com>
2023-12-06 13:59:48 +0000

    deleted redundant files

--- main.py
+++ main.py
@@ -3,9 +3,32 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from filesharer import FileSharer
 
 Builder.load_file('frontend.kv')
 
+# =====================================================================================================
+
+
+class CameraScreen(Screen):
+    # A requirement from kivi
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def capture(self):
+        pass
+
+
+class ImageScreen(Screen):
+    # A requirement from kivi
+    pass
+
+
+# ==================================================================================================
+# Boiler plate code continue:-
 
 class RootWidget(ScreenManager):
     pass
