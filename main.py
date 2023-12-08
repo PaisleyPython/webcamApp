e849f6c436ee2751d8687f2300b27740cfe8e0cb
PaisleyPython <148840962+PaisleyPython@users.noreply.github.com>
2023-12-08 05:26:06 +0000

    Add photo capture/save functionality

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from filesharer import FileSharer
+import time
 
 Builder.load_file('frontend.kv')
 
-# ========================================
+# =======================================
 # By default the texture of camera is equal to the last frame
 
 
@@ -24,7 +25,9 @@ class CameraScreen(Screen):
         self.ids.camera.texture = None
 
     def capture(self):
-        pass
+        current_time = time.strftime('%Y%m%d-%H%M%S')
+        filename = "images/" + current_time + ".png"
+        self.ids.camera.export_to_png(filename)
 
 
 class ImageScreen(Screen):
